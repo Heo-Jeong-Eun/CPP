cdfa4f55a171bfa941f40abe699ef3f5192bd770
J <1001l1000l1110@gmail.com>
2023-12-05 22:47:52 +0900

    Data_Storage_Container

--- Exercise/Data_Storage_Container.cpp
+++ Exercise/Data_Storage_Container.cpp
@@ -6,10 +6,20 @@
 #include <iostream>
 #include <type_traits>
 
+// 가변 인자 템플릿과 std::common_type을 사용하여 임의의 타입의 가변 인자를 받아 std::array로 변환하는 build_array 함수를 정의한다.
+// 이 코드는 가변 인자 템플릿과 std::common_type을 통해 타입 안정성을 유지하면서 임의의 타입을 std::array로 변환하는 일반적인 도구로 사용될 수 있다. 
+
+// Args...는 가변 인자 템플릿으로 임의의 타입들을 받을 수 있다.
 template<typename ... Args>
+
+// std::common_type<Args...>::type : 가변 인자들에 대한 공통 타입을 얻는다.
+// sizeof...(args) : 가변 인자의 개수
+// std::array의 크기는 가변 인자들의 개수와 동일하게 설정된다.
+// std::array를 생성하여 반환한다.
 auto build_array(Args&&... args) -> std::array<typename std::common_type<Args...>::type, sizeof...(args)>
 {
     using commonType = typename std::common_type<Args...>::type;
+    // std::forward<commonType>((Args&&)args)...를 사용하여 각 인자를 commonType으로 변환한다.
     return {std::forward<commonType>((Args&&)args)...};
 }
 
