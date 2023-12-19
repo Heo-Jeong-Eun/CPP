d07950cf059927b0e72fd7c7c81b9b4e66246089
J <1001l1000l1110@gmail.com>
2023-12-19 16:35:29 +0900

    Deleting_Conditional_Elements

--- Exercise/Deleting_Conditional_Elements.cpp
+++ Exercise/Deleting_Conditional_Elements.cpp
@@ -8,27 +8,27 @@
 #include <iostream>
 #include <forward_list>
 
-struct citizen
-{
+struct citizen {
     std::string name;
     int age;
 };
 
-std::ostream &operator<<(std::ostream &os, const citizen &c)
-{
+std::ostream &operator<<(std::ostream &os, const citizen &c) {
     return (os << "[" << c.name << ", " << c.age << "]");
 }
 
-int main()
-{
+int main() {
     std::forward_list<citizen> citizens = {
-            {"Kim", 22}, {"Lee", 25}, {"Park", 18}, {"Jin", 16}
+            {"Kim",  22},
+            {"Lee",  25},
+            {"Park", 18},
+            {"Jin",  16}
     };
 
     auto citizens_copy = citizens; // 깊은 복사
 
     std::cout << "전체 시민: ";
-    for (const auto &c : citizens)
+    for (const auto &c: citizens)
         std::cout << c << " ";
     std::cout << std::endl;
 
@@ -38,7 +38,7 @@ int main()
     });
 
     std::cout << "투표권이 있는 시민: ";
-    for (const auto &c : citizens)
+    for (const auto &c: citizens)
         std::cout << c << " ";
     std::cout << std::endl;
 
@@ -47,7 +47,7 @@ int main()
     });
 
     std::cout << "내년에 투표권이 생기는 시민: ";
-    for (const auto &c : citizens_copy)
+    for (const auto &c: citizens_copy)
         std::cout << c << " ";
     std::cout << std::endl;
 }
