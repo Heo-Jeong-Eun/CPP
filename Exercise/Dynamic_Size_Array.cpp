4e95f410bb5be605d90076bf1cf0cc2ad16b1e80
J <1001l1000l1110@gmail.com>
2023-12-19 16:35:47 +0900

    Dynamic_Size_Array

--- Exercise/Dynamic_Size_Array.cpp
+++ Exercise/Dynamic_Size_Array.cpp
@@ -8,17 +8,16 @@
 #include <algorithm>
 #include <sstream>
 
-template <typename T>
-class dynamic_array
-{
+template<typename T>
+class dynamic_array {
     // 주요 멤버 변수 선언
-    T* data; // 동적 할당의 메모리를 저장하는 변수는 포인터
+    T *data; // 동적 할당의 메모리를 저장하는 변수는 포인터
     size_t n; // 배열의 크기, 동적으로 할당
 
 public:
     // dynamic_array Class의 생성자, 객체가 생성될 때 호출되며 해당 객체의 초기화를 담당한다. int n : 생성자의 매개 변수
-    dynamic_array(int n)
-    {스
+    dynamic_array(int n) {
+        스
         // 포인터를 사용하여 현재 객체의 멤버 변수 n에 접근하고, 생성자의 매개변수 n 값을 해당 멤버 변수에 할당
         // this : 현재 객체의 포인터, this->n : 현재 객체의 멤버 변수 n
         // 생성자의 매개 변수 n을 Class의 멤버 변수 n에 할당한다.
@@ -42,8 +41,7 @@ public:
     // other는 참조(reference)로 선언, const 키워드가 사용되었다.
     // const 키워드는 other 객체를 읽기 전용으로 만든다. 이는 복사 생성자에서 다른 객체의 내용을 변경하지 않겠다는 의미이다.
     // &는 참조(reference)를 나타내며, 복사 생성자에서는 참조를 통해 다른 객체의 내용을 읽어오게 된다.
-    dynamic_array(const dynamic_array<T>& other)
-    {
+    dynamic_array(const dynamic_array<T> &other) {
         // 포인터를 사용하지 않고 직접 현재 Class의 멤버 변수 n에 접근하여, 다른 객체의 멤버 변수 값을 해당 멤버 변수에 할당
         // other 객체의 크기(n)를 현재 객체의 멤버 변수 n에 복사
         // 현재 객체의 크기를 원본 객체와 동일하게 설정하는 역할
@@ -63,24 +61,21 @@ public:
     // 배열의 index에 접근하기 위한 operator[] 함수를 Class에 정의한다. 배열의 특정 index에 대한 참조를 반환한다.
     // 아래 함수는 data 배열에서 index 위치에 있는 요소에 대한 참조를 반환하므로, 이를 통해 해당 요소의 값을 읽거나 수정할 수 있다.
     // non-const 멤버 함수로 정의되어 있어 연산자를 사용하는 객체의 내용을 수정할 수 있다.
-    T& operator[](int index)
-    {
+    T &operator[](int index) {
         return data[index];
     }
 
     // 배열의 특정 index에 대한 상수 참조를 반환한다.
     // data 배열에서 index 위치에 있는 요소에 대한 상수 참조를 반환하므로, 이를 통해 해당 요소의 값을 읽을 수는 있지만, 수정할 수는 없다.
     // const 멤버 함수로 정의되어 있기 때문에, 이 연산자를 사용하는 객체의 내용을 수정할 수 없다.
-    const T& operator[](int index) const
-    {
+    const T &operator[](int index) const {
         return data[index];
     }
 
     // 이 함수는 배열의 특정 index에 해당하는 요소에 참조를 반환한다.
     // 배열의 특정 인덱스에 안전하게 접근할 수 있도록 범위 검사를 수행하는 역할이다.
     // index가 배열의 유효한 범위 내에 있다면 해당 index의 요소에 대한 참조를 반환하고, 그렇지 않으면 "index out of range" 예외를 던집니다.
-    T& at(int index)
-    {
+    T &at(int index) {
         if (index < n)
             return data[index];
         throw "index out of range";
@@ -89,16 +84,14 @@ public:
     // 이 함수는 배열의 크기를 반환한다.
     // size() 함수는 멤버 변수 n을 반환하며, const 키워드가 붙어 있어서 해당 함수 내에서는 멤버 변수의 수정이 허용되지 않는다.
     // size() 함수를 호출하면 배열의 크기를 얻을 수 있다.
-    size_t size() const
-    {
+    size_t size() const {
         return n;
     }
 
     // 이 소멸자는 동적으로 할당된 배열을 삭제하여 메모리 누수를 방지한다.
     // delete[] data;를 통해 data가 가리키고 있는 동적 배열을 해제한다.
     // 이 함수는 객체가 소멸될 때 자동으로 호출되며, 해당 객체가 메모리를 올바르게 해제하도록 한다.
-    ~dynamic_array()
-    {
+    ~dynamic_array() {
         delete[] data; // 메모리 릭 방지
     }
 
@@ -107,22 +100,23 @@ public:
 
     // begin() 함수는 배열의 첫 번째 요소를 가리키는 포인터를 반환
     // const 버전도 정의되어 있어서 const 객체에 대해서도 사용할 수 있다.
-    T* begin() { return data; }
-    const T* begin() const { return data; }
+    T *begin() { return data; }
+
+    const T *begin() const { return data; }
 
     // end() 함수는 배열의 끝을 가리키는 포인터를 반환
     // const 버전도 정의되어 있어서 const 객체에 대해서도 사용할 수 있다.
     // 반환된 포인터를 통해 배열의 끝 지점 다음을 가리키게 되는데, 보통 반복문에서는 시작부터 끝 지점 전까지 순회하게 된다.
-    T* end() { return data + n; }
-    const T* end() const { return data + n; }
+    T *end() { return data + n; }
+
+    const T *end() const { return data + n; }
 
     // dynamic_array 객체를 합치는 연산자 오버로딩 함수이다.
     // 두 배열을 합친 결과를 새로운 배열로 반환한다. 구체적으로는 두 배열을 합쳐서 새로운 배열을 만들고 반환하는 기능을 수행한다.
 
     // operator+ : 이 함수는 두 개의 dynamic_array 객체를 합치는 용도로 정의되었다.
     // friend 키워드를 사용, Class의 friend로 선언되었으므로, 해당 Class의 private 멤버에 접근할 수 있다.
-    friend dynamic_array<T> operator+(const dynamic_array<T>& arr1, dynamic_array<T>& arr2)
-    {
+    friend dynamic_array<T> operator+(const dynamic_array<T> &arr1, dynamic_array<T> &arr2) {
         // 두 배열을 합친 결과를 담을 새로운 dynamic_array 객체 result를 생성
         // 크기는 두 배열의 크기를 합친 크기로 설정
         dynamic_array<T> result(arr1.size() + arr2.size());
@@ -138,8 +132,7 @@ public:
 
     // dynamic_array Class의 멤버 함수로서, 배열의 요소를 문자열로 변환하여 반환하는 to_string 함수를 정의한 부분
     // 함수는 문자열을 구분하는 구분자(separator)를 인자로 받는다. 기본값으로는 쉼표와 공백(", ")이 사용되며, 이는 인자를 생략할 경우에 사용된다.
-    std::string to_string(const std::string& sep = ", ")
-    {
+    std::string to_string(const std::string &sep = ", ") {
         // 배열의 크기 n이 0인 경우, 즉 배열이 비어있는 경우 빈 문자열("")을 반환
         // 빈 배열의 경우 아무런 구분자 없이 빈 문자열을 반환하도록 하는 조건문
         if (n == 0)
@@ -161,8 +154,7 @@ public:
 };
 
 // student 구조체 선언
-struct student
-{
+struct student {
     // 문자열 name과 int형 standard를 선언
     std::string name;
     int standard;
@@ -170,13 +162,11 @@ struct student
 
 // 출력 스트림에 대한 참조(std::ostream& os)와 const student& 형식의 student 객체(s)를 매개변수로 받는다.
 // 반환 값으로 출력 스트림에 대한 참조를 반환한다. 이는 함수 호출을 연쇄적으로 사용할 수 있도록 해준다.
-std::ostream& operator<<(std::ostream& os, const student& s)
-{
+std::ostream &operator<<(std::ostream &os, const student &s) {
     return (os << "[" << s.name << ", " << s.standard << "]");
 }
 
-int main()
-{
+int main() {
     int nStudents;
     std::cout << "1반 학생 수를 입력하세요 : ";
     std::cin >> nStudents;
@@ -185,8 +175,7 @@ int main()
     // 반복문을 통해 입력받은 학생 수만큼 학생의 이름과 나이를 입력받아 class1 배열에 저장한다.
     dynamic_array<student> class1(nStudents);
 
-    for (int i = 0; i < nStudents; i++)
-    {
+    for (int i = 0; i < nStudents; i++) {
         std::string name;
         int standard;
         std::cout << i + 1 << "번째 학생 이름과 나이를 입력하세요 : ";
@@ -195,16 +184,14 @@ int main()
     }
 
     // 배열 크기보다 큰 index의 학생에 접근
-    try
-    {
+    try {
         // 비정상 종료 코드
         // class1[nStudents] = student{"John", 8}; // 예상할 수 없는 동작
 
         // 정상 종료 코드
         class1.at(nStudents) = student{"John", 8}; // 예외 발생
     }
-    catch (...)
-    {
+    catch (...) {
         std::cout << "예외 발생" << std::endl;
     }
 
